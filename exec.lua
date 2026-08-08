70fea96f94229c97fa0706fd8dcefa8190a642b2
meimeivro <sebixlee2@gmail.com>
2026-08-08 20:51:47 +0200

    create our exec.lua
    ble

---
+++ exec.lua
@@ -0,0 +1,4 @@
+pcall(function()
+  game.StarterGui:SetCore("SendNotification",{Title = "Table",Text = "This script is heavily work-in-progress, it won't do anything. Check discord for updates.", Duration = 10})
+  setclipboard("No discord server yet.")
+end)
